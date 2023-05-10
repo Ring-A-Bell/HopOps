36a659b4c2a1a4d3c448200363a72beb158cb6c7
Aditya Ganti <adi10rox@gmail.com>
2023-05-09 22:49:37 -0700

    inject service for recipe component

--- src/app/recipe-page/recipe-page.component.ts
+++ src/app/recipe-page/recipe-page.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
-import { json } from 'body-parser';
-import { NgFor } from '@angular/common';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-page',
@@ -10,20 +8,11 @@ import { NgFor } from '@angular/common';
   styleUrls: ['./recipe-page.component.scss']
 })
 export class RecipePageComponent {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
 
   jsonRecipes: any;
 
-  ngOnInit(): void{
-    var jsonObj: any;
-    this.http.get("http://localhost:8080/app/userrecipes/YPs-zlGU6gwxOAH3O-zWb").subscribe((data) => {
-    console.log(data);
-    jsonObj = JSON.parse(JSON.stringify(data));
-    console.log("This is without\n", jsonObj);
-    this.jsonRecipes = jsonObj[0].recipes;
-    console.log("This is without2\n", this.jsonRecipes);
-    console.log();
-    });
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe((data: any) => this.jsonRecipes = data[0].recipes);
   }
-
 }
