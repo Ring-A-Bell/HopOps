23c95f688475bfeff27a06f82492df792fa8292b
Apple-Pie-For-All <mcdonnell32@gmail.com>
2023-05-11 17:13:59 -0700

    Added sales-page to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipePageComponent } from './components/recipe-page/recipe-page.component';
+import { SalesPageComponent } from './components/sales-page/sales-page.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
   { path: 'my-recipes', component: RecipePageComponent },
+  { path: 'sales', component: SalesPageComponent },
   { path: '', component: HomePageComponent},
   { path: '**', component: PagenotfoundComponent }
 ];
