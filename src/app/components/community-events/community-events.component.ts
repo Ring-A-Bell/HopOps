1ad2d08abccce625db5000bb22b7023964b7cfa0
parth650 <130958174+parth650@users.noreply.github.com>
2023-05-12 20:51:29 -0700

    Added Events under community-events.component.html

--- src/app/components/community-events/community-events.component.ts
+++ src/app/components/community-events/community-events.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-community-events',
   templateUrl: './community-events.component.html',
-  styleUrls: ['./community-events.component.scss']
+  styleUrls: ['./community-events.component.scss'] 
 })
 export class CommunityEventsComponent {
 
