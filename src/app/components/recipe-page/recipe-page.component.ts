c6fcb34c650105aa8faf148dcc9da2ea45e88179
Aditya Ganti <adi10rox@gmail.com>
2023-05-13 00:08:19 -0700

    create new recipe

--- src/app/components/recipe-page/recipe-page.component.ts
+++ src/app/components/recipe-page/recipe-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-page',
@@ -7,12 +8,24 @@ import { RecipeService } from '../../services/recipe.service';
   styleUrls: ['./recipe-page.component.scss']
 })
 export class RecipePageComponent {
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router) { }
 
   jsonRecipes: any;
 
   ngOnInit(): void {
     this.recipeService.getUserRecipes().subscribe((data: any) => this.jsonRecipes = data[0].recipes);
     //this.recipeService.getAllRecipes().subscribe((data: any) =>  this.jsonRecipes = data);
+  }
+
+  createNewRecipe() {
+    this.recipeService.createNewRecipe().subscribe((data) => {
+      var insertedID = data[0]._id;
+      this.recipeService.addRecipeToUserList(insertedID).subscribe((data) => {
+        console.log(data);
+      });
+    });
+    this.router.navigateByUrl('/my-recipes', { skipLocationChange: true }).then(() => {
+      this.router.navigate([this.router.url]);
+    });
   }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -6,19 +6,58 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeService {
-
+  userID: String = "YPs-zlGU6gwxOAH3O-zWb";
   constructor(private http: HttpClient) { }
 
   public getUserRecipes(): Observable<any> {
     var jsonRecipes: any;
-    const userID: String = "YPs-zlGU6gwxOAH3O-zWb";
     var jsonObj: any;
-    return this.http.get("http://localhost:8080/app/userrecipes/" + userID);
+    return this.http.get("http://localhost:8080/app/userrecipes/" + this.userID);
   }
 
   public getAllRecipes(): Observable<any> {
     var jsonRecipes: any;
     var jsonObj: any;
     return this.http.get("http://localhost:8080/app/recipes");
+  }
+
+  public createJSONRecipe(): any {
+    var obj: any = {
+      "title": "Spaghetti Carbonara",
+      "description": "A classic spaghetti carbonara recipe",
+      "image": "https://images.unsplash.com/photo-1554118811-1e0d5826cf09?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "body": "This spaghetti carbonara recipe is creamy, cheesy, and absolutely delicious!",
+      "recipeIngredients": [
+          {
+              "ingredient": "1J1sO3l9Aba4JxKmC4lVv",
+              "quantity": 3
+          },
+          {
+              "ingredient": "jKtX5X3Trp7DhKlFW9Y3T",
+              "quantity": 2
+          },
+          {
+              "ingredient": "m1G2asH2jcQ3zFzRrCxPT",
+              "quantity": 1
+          }
+      ],
+      "favorite": false
+    }
+    return obj;
+  }
+
+  public createNewRecipe(): Observable<any> {
+    var jsonObj: any;
+    jsonObj = this.createJSONRecipe();
+    console.log(jsonObj);
+    return this.http.post("http://localhost:8080/app/recipes", jsonObj);
+  }
+
+  public addRecipeToUserList(newID: any): Observable<any> {
+    var jsonObj: any = {
+      "recipeID": newID
+    }
+    console.log(jsonObj);
+    return this.http.post("http://localhost:8080/app/userrecipes/" + this.userID, jsonObj);
   }
 }
