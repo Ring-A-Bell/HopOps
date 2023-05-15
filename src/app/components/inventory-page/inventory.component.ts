3fe4a9b59dcb88eeb547835f366629c16b824635
NAGA-MANVITH <manvith.701@gmail.com>
2023-05-15 10:59:13 -0700

    Added service routes and create button

--- src/app/components/inventory-page/inventory.component.ts
+++ src/app/components/inventory-page/inventory.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InventoryService } from '../../services/inventory.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inventory',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent {
+  constructor(private InventoryService: InventoryService, private router: Router) { }
 
+  jsonInventories: any;
+
+  ngOnInit(): void {
+    this.InventoryService.getInventory().subscribe((data: any) => this.jsonInventories = data[0].inventories);
+    //this.InventoryService.getAllRecipes().subscribe((data: any) =>  this.jsonInventories = data);
+  }
+
+  createNewInventory() {
+    this.InventoryService.createNewInventory().subscribe((data) => {
+      var insertedID = data[0]._id;
+    });
+    this.router.navigateByUrl('/inventory', { skipLocationChange: true }).then(() => {
+      this.router.navigate([this.router.url]);
+    });
+  }
 }
--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryService {
+  inventoryID: String = "000000000000000000051";
+  ownerID: String = "000000000000000000021";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public getInventory(): Observable<any> {
+    var jsonInventories: any;
+    var jsonObj: any;
+    return this.http.get("http://localhost:8080/app/inventories/" + this.inventoryID);
+  }
+
+  public getUserInventories(): Observable<any> {
+    var jsonInventories: any;
+    var jsonObj: any;
+    return this.http.get("http://localhost:8080/app/inventories/" + this.ownerID);
+  }
+
+  public createJSONInventory(): any {
+    var obj: any = {
+      "ownerID": this.ownerID,
+      "ingredients": [ "TI7OBzZrGmEv6zVQI1vM8", "Cp4opcfHR6Yu6MgYcaK_3"]
+    }
+    return obj;
+  }
+
+  public createNewInventory(): Observable<any> {
+    var jsonObj: any;
+    jsonObj = this.createJSONInventory();
+    console.log(jsonObj);
+    return this.http.post("http://localhost:8080/app/inventories", jsonObj);
+  }
 }
