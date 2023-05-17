680250e3eb7f83c5b22baebc7bf8e7df678e1195
Apple-Pie-For-All <mcdonnell32@gmail.com>
2023-05-17 12:26:34 -0700

    Integrated sale service with sales-page component

--- src/app/components/sales-page/sales-page.component.ts
+++ src/app/components/sales-page/sales-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SalesService } from 'src/app/services/sales.service';
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
   selector: 'app-sales-page',
   templateUrl: './sales-page.component.html',
   styleUrls: ['./sales-page.component.scss']
 })
-export class SalesPageComponent {
+export class SalesPageComponent implements OnInit{
+
+  private user: string = "000000000000000000021";
+
+  public columnsToDisplay: string[] = ['SaleID', 'SaleDate', 'PaymentMethod'];
+  public dataSource = new MatTableDataSource();
+
+  public saleList: any;
+
+  constructor(private router: Router, private $sales: SalesService){
+      $sales.getSaleHistory(this.user).subscribe( (result: any) => {
+        this.saleList = result;
+        console.log("Sales page loaded")
+      })
+  };
+
+  ngOnInit(): void {
+    
+  }
 
 }
