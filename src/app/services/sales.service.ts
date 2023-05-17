e736a4745a00ae3a2d6b95adbbe12d72b3e888e4
Apple-Pie-For-All <mcdonnell32@gmail.com>
2023-05-17 12:25:47 -0700

    Added sale service

--- src/app/services/sales.service.ts
+++ src/app/services/sales.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
+  hostname:string = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //
+  // Returns json list object of sales
+  //
+  getSaleHistory(user: string) {
+    return this.http.get(this.hostname + '/app/saleHistory/' + user);
+  }
 }
