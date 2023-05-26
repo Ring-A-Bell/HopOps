2cf76197d624abe73a2859e23544433b877c39d1
Aditya Ganti <adi10rox@gmail.com>
2023-05-26 12:19:32 -0700

    clean-up

--- src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var App_1 = require("./App");
-var server = new App_1.App().expressApp;
-server.listen(8080, function () {
-    console.log("Listening on port 8080");
-});
