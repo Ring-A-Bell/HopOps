01d217259865855e54c5649bb56195eed1750581
NAGA-MANVITH <manvith.701@gmail.com>
2023-06-04 14:23:46 -0700

    redirection 404 issue fix for inventory page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   { path: 'my-recipes/:id', component: RecipeDetailedPageComponent },
   { path: 'sales', component: SalesPageComponent },
   { path: '#/community-events', component: CommunityEventsComponent },
-  { path: '#/inventory', component:  InventoryComponent},
+  { path: 'inventory', component:  InventoryComponent},
   { path: 'login', component: LoginPageComponent },
   { path: '', component: HomePageComponent},
   { path: '**', component: PagenotfoundComponent }
